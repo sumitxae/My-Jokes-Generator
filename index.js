7aa3047400ba2054f59cf10e179015d2c853bc51
ayush namdev <120575023+Ayush2004namdev@users.noreply.github.com>
2023-09-06 13:32:15 -0700

    shifting to express

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-const http = require('http')
-const fs = require('fs');
-const { send } = require('process');
-function run(a,b){
-    const blue = a.replace("Enter Joke Here!!",b.joke);
-    console.log(blue)
-    return blue;
-}
-const page = fs.readFileSync('./index.html' , 'UTF-8')
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs = require('hbs')
 
-const server = http.createServer((req , res) => {
-    if(req.url == '/'){
-        (async function(){
-            const data = await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Spooky?type=single')
-            .then(response => response.json())
-            .then(dataa => {
-                const newdata = run(page,dataa);
-                res.end(newdata);
-            });
-        })()
-        res.end(page)
-    }
+// app.set('views' , path.join(__dirname ,))
+app.set('view engine' , 'hbs')
+
+app.get('/' , async (req , res )=> {
+        let dets
+        const fet = await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Spooky?type=single')
+        .then(response => response.json()).then(async(data) => {
+            dets = await data
+        })
+        
+        console.log(dets)
+        res.render('index' , {dets} )
+})
+
+app.listen(3000 , () => {
+    console.log('app listning to port 3000')
 })
 
 
-server.listen(3000 , '127.0.0.1' , (err) => {
-    console.log('server running on port 3000')
-})
+
+
+// const http = require('http')
+// const fs = require('fs');
+// const { send } = require('process');
+// function run(a,b){
+//     const blue = a.replace("Enter Joke Here!!",b.joke);
+//     console.log(blue)
+//     return blue;
+// }
+// const page = fs.readFileSync('./index.html' , 'UTF-8')
+
+// const server = http.createServer((req , res) => {
+//     if(req.url == '/'){
+//         (async function(){
+//             const data = await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Spooky?type=single')
+//             .then(response => response.json())
+//             .then(dataa => {
+//                 const newdata = run(page,dataa);
+//                 res.end(newdata);
+//             });
+//         })()
+//         res.end(page)
+//     }
+// })
+
+
+// server.listen(3000 , '127.0.0.1' , (err) => {
+//     console.log('server running on port 3000')
+// })
